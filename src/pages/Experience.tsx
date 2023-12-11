371187ff5f6f0711465dc0b69db5ee7278ecf568
HejSidharth <143037077+HejSidharth@users.noreply.github.com>
2023-12-11 13:00:26 -0600

    added more padding to center on mobile

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,11 +1,11 @@
 
 export default function Experience() {
   return (
-<div className='flex flex-col mt-10 sm:justify-center sm:h-auto h-screen'>
-      <div className='text-4xl font-bold mt-20 ml-0 mr-5 sm:mr-0 sm:ml-12 flex justify-center sm:justify-start'>Experience</div>
+<div className='flex flex-col mt-10 sm:justify-start justify-center sm:h-auto h-screen'>
+      <div className='text-4xl font-bold mt-20 ml-0  sm:ml-12 flex justify-center sm:justify-start'>Experience</div>
       <div className='flex mt-5 justify-start sm:flex-row flex-col items-center sm:items-start gap-5'>
       <a href="https://uiuc.hack4impact.org/">
-      <div className="ml-10 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors duration-200 indicator">
+      <div className="ml-0 sm:ml-10 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors duration-200 indicator">
         <div className="card-body">
           <h2 className="card-title">
             Hack4Impact 
@@ -22,7 +22,7 @@ export default function Experience() {
   </div>
   </a>
   <a href="https://www.linkedin.com/in/sidharth-hejamadi/">
-  <div className="ml-10 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors duration-200">
+  <div className="ml-0 sm:ml-10 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors duration-200">
         <div className="card-body">
           <h2 className="card-title">
             APS Data Technologies
