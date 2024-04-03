7963bbb6c057b346dc29765090f95f2f6c15a6e0
HejSidharth <143037077+HejSidharth@users.noreply.github.com>
2024-04-03 15:40:29 -0500

    project changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-<footer className="footer footer-center p-4 bg-base-100 text-base-content border-b-1 border-base-300 bottom-0  opacity-95 fixed">
+<footer className="footer footer-center p-4  text-base-content border-b-1 border-base-300 bottom-0  opacity-95 fixed">
   <aside>
     <p>Made with ❤️ by <a href="https://github.com/HejSidharth"><span className='font-bold hover:underline'>Sidharth Hejamadi</span></a></p>
   </aside>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 
 export default function Projects() {
   return (
-<div className='flex flex-col mt-10 sm:h-auto h-screen sm:justify-start justify-center mb-10'>
+<div className='flex flex-col mt-10 sm:h-auto h-screen sm:justify-start justify-center mb-24'>
       <div className='text-4xl font-bold mt-20 ml-0 sm:ml-12 flex justify-center sm:justify-start'>Projects</div>
       <div className='flex flex-col sm:flex-row mt-5 justify-start sm:justify-start items-center sm:items-start gap-5'>
       <a href="https://taskmon.netlify.app">
@@ -57,6 +57,62 @@ export default function Projects() {
     </div>
   </div>
   </div>
+
+  <div className='flex flex-col sm:flex-row mt-5 justify-start sm:justify-start items-center sm:items-start gap-5'>
+      <a href="https://bootcamp-calculator.netlify.app">
+      <div className="ml-0 sm:ml-5 card w-96 bg-base-100 shadow-xl  h-52 hover:bg-base-200 transition-colors duration-200 indicator">
+        <div className="card-body">
+          <h2 className="card-title">
+            JavaScript Calculator 
+            <svg className='fill-current' height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"></path>
+            </svg>    
+            </h2>
+          <p>A fully functioning 4 function calculator built on the web!</p>
+          <div className="card-actions justify-end mt-2">
+            <div className="badge badge-neutral">JavaScript</div> 
+            <div className="badge badge-neutral">CSS</div>
+            <div className="badge badge-neutral">HTML</div>
+      </div>
+    </div>
+  </div>
+  </a>
+        <a href="https://bootcamp-myflix.netlify.app">
+      <div className="ml-0 sm:ml-5 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors duration-200 h-52 indicator">
+        <div className="card-body">
+          <h2 className="card-title">
+            Myflix - Netflix Clone 
+            <svg className='fill-current' height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"></path>
+            </svg>    
+            </h2>
+          <p>A Netflix esque landing page clone</p>
+          <div className="card-actions justify-end mt-2">
+            <div className="badge badge-neutral">HTML</div>
+            <div className="badge badge-neutral">CSS</div>
+      </div>
+    </div>
+  </div>
+  </a>
+  <a href="https://bootcamp-resume.netlify.app">
+  <div className="ml-0 sm:ml-5 card w-96 bg-base-100 shadow-xl  hover:bg-base-200 transition-colors h-52 duration-200">
+        <div className="card-body">
+          <h2 className="card-title">
+            HTML Resume
+            <svg className='fill-current' height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"></path>
+            </svg>          
+            </h2>
+          <p>A website dedicated to show resume!</p>
+          <div className="card-actions justify-end mt-2">
+            <div className="badge badge-neutral">HTML</div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  
+
   </div>  
   )
 }
